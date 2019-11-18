a4356f9e561f6dfdd8d4d4e2d8616083f982b24b
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2019-11-18 16:33:24 +0100

    core: use chacha in rdi

--- core/embed/trezorhal/rdi.c
+++ core/embed/trezorhal/rdi.c
@@ -21,61 +21,88 @@
 
 #include <stdbool.h>
 
-#include "chacha20poly1305/ecrypt-sync.h"
-#include "hmac_drbg.h"
+#include "rdi.h"
 #include "rand.h"
 
-#define BUFFER_LENGTH 128
-#define RESEED_AFTER_BYTES 0
+#define MAX(x,y) (((x)>(y))?(x):(y))
 
-static HMAC_DRBG_CTX drbg_ctx;
+void chacha_drbg_init(CHACHA_DRBG_CTX *ctx, const uint8_t entropy[CHACHA_DRBG_SEED_LENGTH]) {
+  uint8_t buffer[MAX(CHACHA_DRBG_KEY_LENGTH, CHACHA_DRBG_IV_LENGTH)] = {0};
+  ECRYPT_keysetup(&ctx->chacha_ctx, buffer, CHACHA_DRBG_KEY_LENGTH * 8, CHACHA_DRBG_IV_LENGTH * 8);
+  ECRYPT_ivsetup(&ctx->chacha_ctx, buffer);
+
+  chacha_drbg_reseed(ctx, entropy);
+}
+
+static void chacha_drbg_update(CHACHA_DRBG_CTX *ctx, const uint8_t data[CHACHA_DRBG_SEED_LENGTH]) {
+  uint8_t buffer[CHACHA_DRBG_SEED_LENGTH] = {0};
+
+  if (data)
+    ECRYPT_encrypt_bytes(&ctx->chacha_ctx, data, buffer, CHACHA_DRBG_SEED_LENGTH);
+  else
+    ECRYPT_keystream_bytes(&ctx->chacha_ctx, buffer, CHACHA_DRBG_SEED_LENGTH);
+
+  ECRYPT_keysetup(&ctx->chacha_ctx, buffer, CHACHA_DRBG_KEY_LENGTH * 8, CHACHA_DRBG_IV_LENGTH * 8);
+  ECRYPT_ivsetup(&ctx->chacha_ctx, buffer + CHACHA_DRBG_KEY_LENGTH);
+}
+
+void chacha_drbg_generate(CHACHA_DRBG_CTX *ctx, uint8_t *output, uint8_t output_length) {
+  ECRYPT_keystream_bytes(&ctx->chacha_ctx, output, output_length);
+  chacha_drbg_update(ctx, NULL);
+  ctx->reseed_counter++;
+}
+
+void chacha_drbg_reseed(CHACHA_DRBG_CTX *ctx, const uint8_t entropy[CHACHA_DRBG_SEED_LENGTH]) {
+  chacha_drbg_update(ctx, entropy);
+  ctx->reseed_counter = 1;
+}
+
+#define BUFFER_LENGTH 64
+#define RESEED_INTERVAL 0
+
+static CHACHA_DRBG_CTX drbg_ctx;
 static uint8_t buffer[BUFFER_LENGTH];
 static uint8_t buffer_index;
-static uint32_t bytes_since_reseed;
 static bool initialized = false;
 static uint8_t session_delay;
 
 static void rdi_reseed(void) {
-  uint8_t entropy[48];
-  random_buffer(entropy, sizeof(entropy));
-  hmac_drbg_reseed(&drbg_ctx, entropy, sizeof(entropy), NULL, 0);
+  uint8_t entropy[CHACHA_DRBG_SEED_LENGTH];
+  random_buffer(entropy, CHACHA_DRBG_SEED_LENGTH);
+  chacha_drbg_reseed(&drbg_ctx, entropy);
 }
 
 void buffer_refill(void) {
-  hmac_drbg_generate(&drbg_ctx, buffer, BUFFER_LENGTH);
+  chacha_drbg_generate(&drbg_ctx, buffer, BUFFER_LENGTH);
 }
 
 static uint32_t random8(void) {
   buffer_index += 1;
-  bytes_since_reseed += 1;
   if (buffer_index >= BUFFER_LENGTH) {
-    if (RESEED_AFTER_BYTES != 0)
-    {
-      bytes_since_reseed += BUFFER_LENGTH;
-      if (bytes_since_reseed > RESEED_AFTER_BYTES) {
-      	rdi_reseed();
-      	bytes_since_reseed = 0;
-      }
-    }
     buffer_refill();
+    if (drbg_ctx.reseed_counter > RESEED_INTERVAL)
+      rdi_reseed();
     buffer_index = 0;
   }
-
   return buffer[buffer_index];
 }
 
 void rdi_regenerate_session_delay(void) { session_delay = random8(); }
 
 void rdi_start(void) {
-  rdi_reseed();
-  bytes_since_reseed = 0;
+  uint8_t entropy[CHACHA_DRBG_SEED_LENGTH];
+  random_buffer(entropy, CHACHA_DRBG_SEED_LENGTH);
+  chacha_drbg_init(&drbg_ctx, entropy);
   buffer_refill();
   buffer_index = 0;
   initialized = true;
   rdi_regenerate_session_delay();
 }
 
-void rdi_stop(void) { initialized = false; }
+void rdi_stop(void) {
+  initialized = false;
+  memzero(&drbg_ctx, sizeof(drbg_ctx));
+}
 
 void rdi_handler(void) {
   if (initialized) {
--- core/embed/trezorhal/rdi.h
+++ core/embed/trezorhal/rdi.h
@@ -17,7 +17,27 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifndef __TREZORHAL_RDI_H__
+#define __TREZORHAL_RDI_H__
+
+#include "chacha20poly1305/ecrypt-sync.h"
+#include "memzero.h"
+
+#define CHACHA_DRBG_KEY_LENGTH 16
+#define CHACHA_DRBG_IV_LENGTH 8
+#define CHACHA_DRBG_SEED_LENGTH (CHACHA_DRBG_KEY_LENGTH + CHACHA_DRBG_IV_LENGTH)
+
+typedef struct _CHACHA_DRBG_CTX {
+  ECRYPT_ctx chacha_ctx;
+  uint32_t reseed_counter;
+} CHACHA_DRBG_CTX;
+
+void chacha_drbg_init(CHACHA_DRBG_CTX *ctx, const uint8_t entropy[CHACHA_DRBG_SEED_LENGTH]);
+void chacha_drbg_reseed(CHACHA_DRBG_CTX *ctx, const uint8_t entropy[CHACHA_DRBG_SEED_LENGTH]);
+void chacha_drbg_generate(CHACHA_DRBG_CTX *ctx, uint8_t *output, uint8_t output_length);
+
 void rdi_handler(void);
 void rdi_start(void);
 void rdi_stop(void);
 void rdi_regenerate_session_delay(void);
+#endif
